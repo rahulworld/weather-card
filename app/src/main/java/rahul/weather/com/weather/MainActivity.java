3a3fab2cf9e083356cc8848d66d89a26a947bb81
rahulworld <chauhanr371@gmail.com>
2016-11-23 14:25:55 +0530

    added wheather api

--- app/src/main/java/rahul/weather/com/weather/MainActivity.java
+++ app/src/main/java/rahul/weather/com/weather/MainActivity.java
@@ -10,14 +10,14 @@ public class MainActivity extends AppCompatActivity {
 
     TextView cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, weatherIcon, updatedField;
 
-    Typeface weatherFont;
+    //Typeface weatherFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weathericons-regular-webfont.ttf");
+        //weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weathericons-regular-webfont.ttf");
         cityField = (TextView)findViewById(R.id.city_field);
         updatedField = (TextView)findViewById(R.id.updated_field);
         detailsField = (TextView)findViewById(R.id.details_field);
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         humidity_field = (TextView)findViewById(R.id.humidity_field);
         pressure_field = (TextView)findViewById(R.id.pressure_field);
         weatherIcon = (TextView)findViewById(R.id.weather_icon);
-        weatherIcon.setTypeface(weatherFont);
+        //weatherIcon.setTypeface(weatherFont);
 
         function.placeIdTask asyncTask =new function.placeIdTask(new function.AsyncResponse() {
             public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String weather_iconText, String sun_rise) {
