6572d9bd2f91674014eed283e730b08c236ded86
rahulworld <chauhanr371@gmail.com>
2016-11-23 14:19:08 +0530

    submit request

--- app/src/main/java/rahul/weather/com/weather/MainActivity.java
+++ app/src/main/java/rahul/weather/com/weather/MainActivity.java
@@ -3,6 +3,7 @@ package rahul.weather.com.weather;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
         pressure_field = (TextView)findViewById(R.id.pressure_field);
         weatherIcon = (TextView)findViewById(R.id.weather_icon);
         weatherIcon.setTypeface(weatherFont);
+
+        function.placeIdTask asyncTask =new function.placeIdTask(new function.AsyncResponse() {
+            public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String weather_iconText, String sun_rise) {
+
+                cityField.setText(weather_city);
+                updatedField.setText(weather_updatedOn);
+                detailsField.setText(weather_description);
+                currentTemperatureField.setText(weather_temperature);
+                humidity_field.setText("Humidity: "+weather_humidity);
+                pressure_field.setText("Pressure: "+weather_pressure);
+                weatherIcon.setText(Html.fromHtml(weather_iconText));
+
+            }
+        });
+        asyncTask.execute("25.180000", "89.530000");
     }
+
 }
