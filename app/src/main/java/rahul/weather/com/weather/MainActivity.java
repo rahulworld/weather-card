2948b9ccb186f4f1840169212a42f31bb205f56e
rahulworld <chauhanr371@gmail.com>
2016-11-24 20:18:53 +0530

    final changed

--- app/src/main/java/rahul/weather/com/weather/MainActivity.java
+++ app/src/main/java/rahul/weather/com/weather/MainActivity.java
@@ -1,14 +1,33 @@
 package rahul.weather.com.weather;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
     TextView cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, weatherIcon, updatedField;
+    // Location Variables
+    private LocationManager locationManager;
+    private final static int DISTANCE_UPDATES = 10;
+    private final static int TIME_UPDATES = 20;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private boolean LocationAvailable;
+    String latitude,longitude;
+    Button takeButton;
 
     //Typeface weatherFont;
 
@@ -24,9 +43,18 @@ public class MainActivity extends AppCompatActivity {
         humidity_field = (TextView)findViewById(R.id.humidity_field);
         pressure_field = (TextView)findViewById(R.id.pressure_field);
         weatherIcon = (TextView)findViewById(R.id.weather_icon);
+        takeButton= (Button) findViewById(R.id.show_weather);
         //weatherIcon.setTypeface(weatherFont);
+        LocationAvailable = false;
 
-        function.placeIdTask asyncTask =new function.placeIdTask(new function.AsyncResponse() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (checkPermission()) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIME_UPDATES, DISTANCE_UPDATES, this);
+        } else {
+            requestPermission();
+        }
+
+        final function.placeIdTask asyncTask =new function.placeIdTask(new function.AsyncResponse() {
             public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String weather_iconText, String sun_rise) {
 
                 cityField.setText("City : "+weather_city);
@@ -39,7 +67,118 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        asyncTask.execute("28.613900", "77.209000");
+        takeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(latitude=="" && longitude==""){
+                    //display in short period of time
+                    Toast.makeText(getApplicationContext(), "Wait Until Location in Degrees", Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    asyncTask.execute(latitude,longitude);
+                }
+            }
+        });
     }
 
+    /**
+     * Monitor for location changes
+     * @param location holds the new location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView latitudeText = (TextView)findViewById(R.id.latitude);
+        TextView longitudeText = (TextView)findViewById(R.id.longitude);
+        latitudeText.setText("Latitude : " + String.valueOf(location.getLatitude()));
+        longitudeText.setText("Longitude : "+ String.valueOf(location.getLongitude()));
+        latitude=String.valueOf(location.getLatitude());
+        longitude=String.valueOf(location.getLongitude());
+    }
+
+    /**
+     * GPS turned off, stop watching for updates.
+     * @param provider contains data on which provider was disabled
+     */
+    @Override
+    public void onProviderDisabled(String provider) {
+        if (checkPermission()) {
+            locationManager.removeUpdates(this);
+        } else {
+            requestPermission();
+        }
+    }
+
+    /**
+     * GPS turned back on, re-enable monitoring
+     * @param provider contains data on which provider was enabled
+     */
+    @Override
+    public void onProviderEnabled(String provider) {
+        if (checkPermission()) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIME_UPDATES, DISTANCE_UPDATES, this);
+        } else {
+            requestPermission();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+    private boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED){
+            LocationAvailable = true;
+            return true;
+        } else {
+            LocationAvailable = false;
+            return false;
+        }
+    }
+
+    /**
+     * Request permissions from the user
+     */
+    private void requestPermission(){
+
+        /**
+         * Previous denials will warrant a rationale for the user to help convince them.
+         */
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this, "This app relies on location data for it's main functionality. Please enable GPS data to access all features.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    /**
+     * Monitor for permission changes.
+     *
+     * @param requestCode passed via PERMISSION_REQUEST_CODE
+     * @param permissions list of permissions requested
+     * @param grantResults the result of the permissions requested
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    /**
+                     * We are good, turn on monitoring
+                     */
+                    if (checkPermission()) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIME_UPDATES, DISTANCE_UPDATES, this);
+                    } else {
+                        requestPermission();
+                    }
+                } else {
+                    /**
+                     * No permissions, block out all activities that require a location to function
+                     */
+                    Toast.makeText(this, "Permission Not Granted.", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
 }
